37718bb262cf65f37a2929b256cee5a8ef6b6aba
onamfc <onamfc@gmail.com>
2025-01-16 22:05:11 -0800

    adjust next settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "api.breakingmedia.ai",
+                pathname: "/storage/blog-images/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
