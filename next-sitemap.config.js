6bf3214f67278f195e0c2bc335509e66232e2630
onamfc <onamfc@gmail.com>
2025-01-22 18:54:53 -0800

    add sitemap? g

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://breakingmedia.ai', // Replace with your domain
+    generateRobotsTxt: true, // Generate a robots.txt file
+    exclude: ['/admin/**'], // Exclude private pages
+    changefreq: 'daily', // Frequency of content updates
+};
