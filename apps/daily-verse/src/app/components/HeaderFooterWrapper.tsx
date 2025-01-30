374601c9b0612fab6793a3f431b38b68e348d5fa
Estrella <onamfc@gmail.com>
2025-01-29 17:58:05 -0800

    update logo route

--- apps/daily-verse/src/app/components/HeaderFooterWrapper.tsx
+++ apps/daily-verse/src/app/components/HeaderFooterWrapper.tsx
@@ -9,7 +9,7 @@ export default function HeaderFooterWrapper({children}: { children: React.ReactN
     const pathname = usePathname();
     const shouldShowHeaderFooter = pathname !== "/";
 
-    const route = "/scriptures";
+    const route = "/scripture";
     const width = 260;
     const DailVerseSVG = DailVerseLogo(); // Call the BreakingMediaLogo function to get the SVG string
 
