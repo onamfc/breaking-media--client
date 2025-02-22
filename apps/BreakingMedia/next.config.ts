9f00e1724782ea1064cdf90aea4cbc4d7456fc1e
onamfc <onamfc@gmail.com>
2025-02-21 21:46:56 -0800

    redirect home to articles

--- apps/BreakingMedia/next.config.ts
+++ apps/BreakingMedia/next.config.ts
@@ -15,6 +15,15 @@ const nextConfig: NextConfig = {
             },
         ],
     },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/articles',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
